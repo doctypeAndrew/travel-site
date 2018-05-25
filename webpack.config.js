2fa33b3ec1fc3d4397913bc972d4977dea99d4f1
Andrei <iaconi.andrei@yahoo.com>
2018-05-25 09:49:30 +0300

    completed lazy loading

--- webpack.config.js
+++ webpack.config.js
@@ -4,17 +4,20 @@ const path = require('path');
 module.exports = {
     mode: 'production',
   // set this to your entry point
-  entry: "./app/assets/scripts/App.js",
+  entry: {
+    App: "./app/assets/scripts/App.js",
+    Vendor: "./app/assets/scripts/Vendor.js"
+  },
 
   // change this to your output path
   output: {
     path: path.resolve(__dirname,'./app/temp/scripts'),
-    filename: "App.js",
+    filename: "[name].js",
     publicPath: "/assets/"
   },
 
   // create a map file for debugging
-  devtool: 'source-map',
+  //devtool: 'source-map',
 
   // configure the loaders
   module: {
